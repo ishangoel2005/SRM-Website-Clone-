c81dd9d522218da697e891e534b4530b58ed522b
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-13 00:33:54 +0530

    campus added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 
 
@@ -17,7 +18,8 @@ const App = () => {
       <Programs/>
       <About/>
       <Title subTitle='Gallary' title='Campus Photos'/>
-
+      <Campus/>
+      <Title subTitle='Testimonials' title='What Students Say?'/>
       </div>  
     </div>
   )

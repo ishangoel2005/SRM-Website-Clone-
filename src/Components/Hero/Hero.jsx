1cedb71f191bf399b1414f60b48cf453d8955769
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-12 19:32:27 +0530

    improved

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,8 +3,12 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-
+    <div className='hero container'>
+      <div className='hero-text'>
+      <h1>Learn-Leap-Lead</h1>
+      <p>SRMIST’s engineering programs endeavor to be at the forefront of innovation. They also foster multi-disciplinary collaborations aimed at solving the most pressing global problems. Our mission is to seek solutions to global challenges by using the power of engineering principles, techniques and systems. We believe that engineers should not only possess deep technical excellence, but also nurture creativity, cultural awareness and entrepreneurial skills that come from exposure to science, business, medicine and other disciplines</p>
+      <button className='btn'>Explore More</button>
+      </div>
     </div>
   )
 }

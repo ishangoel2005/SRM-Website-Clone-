2cd54e0089653f71dedbbcb6eb16cb224250248e
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-12 16:15:12 +0530

    improved

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
-const Navbar = () => {
+const navbar = () => {
   return (
-    <nav>
-        <img src={logo} alt="" />
+    <nav className='container'>
+        <img src={logo} alt="" className='logo' />
         <ul>
             <li>Home</li>
             <li>Program</li>
             <li>About Us</li>
             <li>Campus</li>
             <li>Testimonials</li>
-            <li>Contact Us</li>
+            <li> <button className='btn'>Contact Us</button></li>
         </ul>
     </nav>
   )
 }
 
-export default Navbar
+export default navbar

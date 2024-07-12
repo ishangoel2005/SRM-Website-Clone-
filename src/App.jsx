28a7372f65d37d72e9b65e552f01cf7a16ac7ab4
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-12 19:24:19 +0530

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Navbar/Hero/Hero'
+import Hero from './Components/Hero/Hero'
+
 
 
 const App = () => {

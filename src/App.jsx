369f0f4339723a7d4c0838b476aaa2bb46914048
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-13 18:54:51 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 
 
@@ -26,6 +27,7 @@ const App = () => {
       <Title subTitle='Contact Us' title='Get in Touch'/>
       </div>  
       <Contact/>
+      <Footer/>
     </div>
   )
 }
